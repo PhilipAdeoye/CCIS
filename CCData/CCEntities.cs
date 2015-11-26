53b6f331b512426b2d10e513ac342bb04ff66d8c
Philip <oadeoye@bitbucket.org>
2015-11-25 18:25:57 -0600

    First commit: Adding CCData and FumeHood projects. CCData is all about handling database interactions while FumeHood is a project for testingstuff that I'm not sure works (but that's the point of testing, isn't it?)

---
+++ CCData/CCEntities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Validation;
+using System.Data.Entity.Infrastructure;
+
+
+namespace CCData
+{
+    public partial class CCEntities
+    {
+        #region SaveChanges
+        public override int SaveChanges(System.Data.Objects.SaveOptions options)
+        {
+            DetectChanges();
+            foreach (var entry in ObjectStateManager.GetObjectStateEntries(System.Data.EntityState.Added | System.Data.EntityState.Modified))
+            {
+                if (!entry.IsRelationship)
+                {
+                    UpdateTrackedEntity(entry);
+                }
+            }
+
+            return base.SaveChanges(options);
+        } 
+        #endregion
+
+        #region UpdateTrackedEntity
+        private void UpdateTrackedEntity(System.Data.Objects.ObjectStateEntry entry)
+        {
+            if (entry.State == System.Data.EntityState.Added)
+            {
+                var entity = entry.Entity as ICreatableEntity;
+                if (entity != null)
+                {
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+            }
+            else if (entry.State == System.Data.EntityState.Modified)
+            {
+                var entity = entry.Entity as IModifiableEntity;
+                if (entity != null)
+                {
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+            }
+        } 
+        #endregion
+    }
+}
---
+++ CCData/Organization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace CCData
+{
+    [MetadataType(typeof(Organization.MetaData))]
+    public partial class Organization: IValidatableObject, ICreatableEntity, IModifiableEntity
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            using (var db = new CCEntities())
+            {
+                if (db.Organizations.Any(o => o.Name == Name && o.OrganizationId != OrganizationId))
+                    yield return new ValidationResult("Name must be unique", new[] { "Name" });
+            }
+        }
+
+        #region MetaData
+        private sealed class MetaData
+        {
+            [Required(ErrorMessage = "Organization Name is required.")]
+            public string Name { get; set; }
+        }
+        #endregion
+    }
+}
